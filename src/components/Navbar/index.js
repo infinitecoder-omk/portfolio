d3bebb27fccd91f4a2d116ee7739d19ceb770ecc
localpoc <omvrm4040@gmail.com>
2025-07-29 19:30:56 +0530

    changed button position

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -33,10 +33,10 @@ const Navbar = ({ darkMode, toggleDarkMode }) => {
           <NavLink href='#education'>Education</NavLink>
         </NavItems>
         <ButtonContainer>
-          <GitHubButton href={Bio.github} target="_blank">Github Profile</GitHubButton>
+          <GitHubButton href={Bio.github} target="_blank">Github</GitHubButton>
              
         <span onClick={toggleDarkMode} style={{ marginLeft: "auto" }}>
-        {darkMode ? <FcNoIdea style={{ fontSize: "28px" }}/> : <FcIdea style={{ fontSize: "28px" }}/>}
+        {darkMode ? <FcIdea style={{ fontSize: "28px" }}/> : <FcNoIdea style={{ fontSize: "28px" }}/>}
       </span>
         </ButtonContainer>
         {
